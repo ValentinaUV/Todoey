2abe6772f02b705053c4214fde70334cd1e71efe
Valentina <galaxyspevv@gmail.com>
2021-12-19 00:56:53 +0200

    cleanup

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -27,7 +27,6 @@ class TodoListViewController: SwipeTableViewController {
         tableView.separatorStyle = .none
     }
     
-    
     override func viewWillAppear(_ animated: Bool) {
         
         if let colorHex = selectedCategory?.color {
